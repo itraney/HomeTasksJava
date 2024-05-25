1d11a7e42e60f8ccfdd80420e44a43daf5bc3be6
Валентович Ян <VAlentovican@gmail.com>
2024-05-25 14:50:12 +0300

    ДЗ по занятию 1

--- src/main/java/Lesson1/HelloWorld.java
+++ src/main/java/Lesson1/HelloWorld.java
@@ -1,20 +1,7 @@
 package Lesson1;
 
-import java.io.Console;
-import java.util.Scanner;
-
 public class HelloWorld {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Please, enter your name: ");
-        String name = in.next();
-
-        System.out.printf("Hello World " + name);
-        in.close();
-
-//        String name;
-//        Console con = System.console();
-//        name = con.readLine("Please, enter your name: ");
-//        System.out.println("Hello World, " + name);
+        System.out.printf("Hello World");
     }
 }
--- src/main/java/Lesson1/HelloWorldAndName.java
+++ src/main/java/Lesson1/HelloWorldAndName.java
@@ -1,4 +1,14 @@
 package Lesson1;
 
+import java.util.Scanner;
+
 public class HelloWorldAndName {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please, enter your name: ");
+        String name = in.next();
+
+        System.out.printf("Hello World " + name);
+        in.close();
+    }
 }
