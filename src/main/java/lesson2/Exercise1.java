08c06a4510a76908487e1197fa2ec06f2f1ed44c
Валентович Ян <VAlentovican@gmail.com>
2024-05-26 00:07:59 +0300

    Упражнение 1 с дополнениями (для помощи использовался chat GPT, но исправления были в основном по стилистике + доп валидацию добавил

--- src/main/java/lesson2/Exercise1.java
+++ src/main/java/lesson2/Exercise1.java
@@ -6,29 +6,34 @@ public class Exercise1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Please, enter number: ");
-        int numberX = in.nextInt();
 
-        //Проверка на валидность данных в диапозоне
-       if (numberX < -999 || numberX > 999) {
-           System.out.println("Invalid input. Please enter a number between -999 and 999");
-       //Проверка основных сценариев
-       } else {
-           if (numberX < -99) {
-               System.out.println("Трехзначное отрицательное");
-           } else if (numberX >= -99 && numberX <= -10) {
-               System.out.println("Двузначное отрицательное");
-           } else if (numberX > -10 && numberX < 0) {
-               System.out.println("Однозначное отрицательное");
-           } else if (numberX == 0) {
-               System.out.println("Ноль");
-           } else if (numberX > 99) {
-               System.out.println("Трехзначное положительное");
-           } else if (numberX > 9 && numberX <= 99) {
-               System.out.println("Двузначное положительное");
-           } else if (numberX >= 0 && numberX <= 9) {
-               System.out.println("Однозначное положительное");
-           }
-       }
+        if (in.hasNextInt()) {
+            int numberX = in.nextInt();
+
+            //Проверка на валидность данных в диапозоне
+            if (numberX < -999 || numberX > 999) {
+                System.out.println("Invalid input. Please enter a number between -999 and 999");
+                //Проверка основных сценариев
+            } else {
+                if (numberX < -99) {
+                    System.out.println("Трехзначное отрицательное");
+                } else if (numberX >= -99 && numberX <= -10) {
+                    System.out.println("Двузначное отрицательное");
+                } else if (numberX > -10 && numberX < 0) {
+                    System.out.println("Однозначное отрицательное");
+                } else if (numberX == 0) {
+                    System.out.println("Ноль");
+                } else if (numberX > 99) {
+                    System.out.println("Трехзначное положительное");
+                } else if (numberX > 9 && numberX <= 99) {
+                    System.out.println("Двузначное положительное");
+                } else if (numberX >= 0 && numberX <= 9) {
+                    System.out.println("Однозначное положительное");
+                }
+            }
+        } else {
+            System.out.println("Invalid input. Please enter a valid integer.");
+        }
         in.close();
     }
 }
