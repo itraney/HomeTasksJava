0a9e9b0c9b0879b404117a05b3058e687feea9dc
Валентович Ян <VAlentovican@gmail.com>
2024-05-27 20:26:13 +0300

    исправление замечаний преподавателя

--- src/main/java/Lesson1/HelloWorld.java
+++ src/main/java/Lesson1/HelloWorld.java
@@ -2,6 +2,6 @@ package Lesson1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.printf("Hello World");
+        System.out.print("Hello World");
     }
 }
